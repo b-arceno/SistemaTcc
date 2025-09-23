d789f0bc5b0cb7b6fef6db7e357fdf56ad295b2d
Brenda Arceno <brenda359rodrigues@gmail.com>
2025-09-23 07:38:14 -0300

    erro corrigido - 'Node_modules' estava errado e foi reisntalado

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const session = require('express-session');
-const db = require('../config/database');
+const db = require('./config/database');
 
 const authRoutes = require('./routes/index');
 const lojaRoutes = require('./routes/lojaRoutes');
